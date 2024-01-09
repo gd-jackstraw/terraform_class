0d2438477195aba506cea24ca592c1cdbf6f648c
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-08 21:01:09 -0500

    Update virtualnetwork.tf

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -10,7 +10,19 @@ locals {
 
 resource "azurerm_virtual_network" "virtual-network-group" {
   for_each            = {for list in local.list_of_vn: list=>list}
-    name                = "${var.prefix}list"
+  name                = "${var.prefix}list"
+  location            = azurerm_resource_group.tf-rg-philippe.location
+  resource_group_name = azurerm_resource_group.tf-rg-philippe.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_virtual_network" "linux-vm-virtualnetwork" {
+  name                = "linux-vm-vn>
   location            = azurerm_resource_group.tf-rg-philippe.location
   resource_group_name = azurerm_resource_group.tf-rg-philippe.name
   address_space       = ["10.0.0.0/16"]

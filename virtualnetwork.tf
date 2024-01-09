4b351621fb8b6978745b317c5ae42ea04d1bdc69
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-08 20:52:46 -0500

    Update virtualnetwork.tf

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -16,17 +16,6 @@ resource "azurerm_virtual_network" "virtual-network-group" {
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.vn-sg1.id
-  }
-
   tags = {
     environment = "Production"
   }

cfe59b0b0129c6f99c4efc92d7296a9a24725015
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-08 20:26:29 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -18,8 +18,8 @@ resource "azurerm_storage_account" "tf-storageaccount" {
 resource "azurerm_storage_account" "azurermmcitcountexample" {
   count=5
   name                     = "${var.myname}storage${count.index}"
-  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
-  location                 = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name      = azurerm_resource_group.tf-rg-philippe.name
+  location                 = azurerm_resource_group.tf-rg-philippe.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
   access_tier=var.access_tier

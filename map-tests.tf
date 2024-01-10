98d964c7cb8e12f0e5e50a58017b54a0eea84093
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-10 18:01:44 -0500

    Update map-tests.tf

--- map-tests.tf
+++ map-tests.tf
@@ -3,13 +3,13 @@ locals {
   enemies_destroyed = [4252, 900, 29320984098]
 
 map = {
- for index, character in toset(local.characters) :
+ for index, character in (local.characters) :
   #Convert chracter list to a set
   character => local.enemies_destroyed[index]
  }
 }
 
 output "starwars_char_deaths"{
- value= [for index, character in (local.characters):character => local.enemies_destroyed[index]]
+ value= local.map
 }
 

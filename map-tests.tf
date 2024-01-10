c1327b6e9e7444660f05f035d04a99cac39bd3f9
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-10 18:37:25 -0500

    Update map-tests.tf

--- map-tests.tf
+++ map-tests.tf
@@ -13,3 +13,17 @@ output "starwars_char_deaths"{
  value= local.map
 }
 
+
+variable "simple_nested_list" {
+  type = list(list(string))
+  default = [["a","b"], ["c","d"], ["e","f"]]
+
+locals {
+simple_local_flattened_list = flatten(var.simple_nested_list)}
+
+output "simple_flattened_list_result" {
+value = local.simple_local_flattened_list
+}
+
+output "simple_nested_list_result" {
+value = var.simple_nested_list

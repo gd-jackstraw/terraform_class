b7407ade6db58728e7a76a3437ce81354580ce90
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-12 18:26:36 -0500

    Update yaml-example.tf

--- yaml-example.tf
+++ yaml-example.tf
@@ -1,7 +1,7 @@
 locals {
   linux_app = [for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   linux_app_list = flatten([
-      for app in local.linux.app : [  
+      for app in local.linux_app : [  
         for linuxapps in try(app.listoflinuxapp, {}) : {
           name=linuxapps.name
           ostype=linuxapps.os_type
@@ -10,3 +10,7 @@ locals {
         ]
       ])
     }
+
+output "list_of_linux_app" {
+value = local.linux_app_list
+}

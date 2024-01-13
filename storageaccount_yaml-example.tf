2b8e6a23a25db488ab078479d2023e09bec0d104
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-12 20:21:37 -0500

    Update storageaccount_yaml-example.tf

--- storageaccount_yaml-example.tf
+++ storageaccount_yaml-example.tf
@@ -18,6 +18,8 @@ resource "azurerm_storage_account" "storageaccount-yaml-example" {
   name                = each.value.name
   resource_group_name = azurerm_resource_group.tf-rg-philippe.name
   location            = azurerm_resource_group.tf-rg-philippe.location
+        account_tier=each.value.account_tier
+        account_replication_type=each.value.account_replication_type    
   tags = {
     environment = "staging"
   }

9e40b59b8664d54859225dbd000bf3bc4302ebcc
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-12 19:45:13 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -19,7 +19,7 @@ locals{
  storageaccounts=[for f in fileset("${path.module}/configs/storageaccount_configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/storageaccount_configs/${f}"))]
  storageaccountlist = flatten([
     for info in local.storageaccounts : [
-      for storageaccount in try(data.listofstorageaccount, []) :{
+      for storageaccount in try(info.listofstorageaccount, []) :{
         name=storageaccount.name
         account_tier=storageaccount.account_tier
         account_replication_type=storageaccount.account_replication_type    

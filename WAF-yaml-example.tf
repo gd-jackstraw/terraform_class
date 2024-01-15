9252de154472116e031b35415edbe6455368a577
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-15 18:56:30 -0500

    Update WAF-yaml-example.tf

--- WAF-yaml-example.tf
+++ WAF-yaml-example.tf
@@ -1,11 +1,11 @@
 locals{
- waf-vm_0 =[for f in fileset("${path.module}/configs/waf-config", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/waf-config/${f}"))]
+ waf_vm_0 =[for f in fileset("${path.module}/configs/waf-config", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/waf-config/${f}"))]
  waf-list = flatten([
-    for info in local.waf-vm_0 : [
+    for info in local.waf_vm_0 : [
       for waf-1 in try(info.wafpolicy, []) :{
-        name=waf-vm_0.name
-        resource_group_name=waf-vm0_.resource_group_name
-        location=waf-vm_0.location
+        name=waf_vm_0.name
+        resource_group_name=waf_vm_0.resource_group_name
+        location=waf_vm_0.location
       }
     ]
   ])
@@ -14,7 +14,7 @@ locals{
 
 resource "azurerm_web_application_firewall_policy" "waf-windowsvm" {
 
-  for_each            ={for waf in local.waf-vm_0: "${waf.name}"=>waf }
+  for_each            ={for waf in local.waf_vm_0: "${waf.name}"=>waf }
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
   location            = each.value.location

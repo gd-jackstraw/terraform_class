06ce7cd806a635ebdf0694c4a16efbe1b44f99bd
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-15 19:00:11 -0500

    Update WAF-yaml-example.tf

--- WAF-yaml-example.tf
+++ WAF-yaml-example.tf
@@ -3,9 +3,9 @@ locals{
  waf-list = flatten([
     for info in local.waf_vm_0 : [
       for waf-1 in try(info.wafpolicy, []) :{
-        name=waf_vm_0.name
-        resource_group_name=waf_vm_0.resource_group_name
-        location=waf_vm_0.location
+        name=waf-1.name
+        resource_group_name=waf-1.resource_group_name
+        location=waf-1.location
       }
     ]
   ])
@@ -14,7 +14,7 @@ locals{
 
 resource "azurerm_web_application_firewall_policy" "waf-windowsvm" {
 
-  for_each            ={for waf in local.waf_vm_0: "${waf.name}"=>waf }
+  for_each            ={for waf in local.waf-list: "${waf.name}"=>waf }
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
   location            = each.value.location

87911ad731763d81eb429cb02fd9eb128e98ba3d
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-15 19:56:14 -0500

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,5 +1,5 @@
-locals {
-  loadbalancer=[for f in fileset("${path.module}/config/loadbalancer-config", "[^_]*.yaml") : yamldecode(file("${path.module}/config/loadbalancer_config/${f}"))]
+locals {  
+loadbalancer =[for f in fileset("${path.module}/configs/loadbalancer-config/", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/loadbalancer-config/${f}"))]
   lb_list = flatten([
     for lb in local.loadbalancer : [
       for lb-config in try(lb.loadbalancer_config, []) :{

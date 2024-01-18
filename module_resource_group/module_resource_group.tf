24039b6841031cf10251d0fd6e6d85a7ea08485d
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-17 19:37:27 -0500

    Create module_resource_group.tf

---
+++ module_resource_group/module_resource_group.tf
@@ -0,0 +1,15 @@
+#module for resource group
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+}
+
+variable "location" {
+  description = "The Azure region where the storage account will be created"
+  default     = "eastus"
+
+resource "azurerm_resource_group" "module-example-rg" {
+    name                     = var.resource_group_name
+    location                 = var.location
+
+}

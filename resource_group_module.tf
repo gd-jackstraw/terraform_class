239379848dd5c2d36c202dbdfbfdd42884575126
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-17 19:37:22 -0500

    Update resource_group_module.tf

--- resource_group_module.tf
+++ resource_group_module.tf
@@ -1,7 +1,11 @@
 module "resource_group" {
     source = "./module_resource_group/"
 
-      name     = "module_RG"
+      resource_group_name     = "module_RG"
       location = "eastus"
 
   }
+
+output "rg-module-name" {
+    value=module.resource_group.resource_group_name 
+}

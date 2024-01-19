fba658b9ae0a4abefa02896959c3e6cec8256267
gd-jackstraw <143850151+gd-jackstraw@users.noreply.github.com>
2024-01-19 18:15:57 -0500

    Update linux_webapp_module.tf

--- linux_webapp_module.tf
+++ linux_webapp_module.tf
@@ -1,11 +1,1 @@
-module "linux_webapp_module_ex" {
-  source="./module_linux_webapp/"
-    linux_app_name="module_linux_app"
-    os_type="Linux"
-    sku_name="P1v2"
 
-}
-
-output "linux_module_name" {
-  value=module.linux_webapp_module_ex.linux_app_name
-}
